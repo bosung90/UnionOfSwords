d1fb03c8594324dabc66cac5af3766eb1ff3c45b
Lisa Wong <miss.lisa19@hotmail.com>
2015-02-11 22:29:47 -0800

    script for move towards base

---
+++ Assets/Scripts/MoveTowardsBase.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveTowardsBase : MonoBehaviour {
+
+	public GameObject castle;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 currentPos = transform.position;
+		Vector3 castlePos = castle.transform.position;
+		float step = speed * Time.deltaTime;
+		currentPos = Vector3.MoveTowards (currentPos, castlePos, step);
+	}
+}
