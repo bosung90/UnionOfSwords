3bf7efac769653d6d4fc41ecf27df22fcc52b145
Lisa Wong <miss.lisa19@hotmail.com>
2015-02-11 23:44:45 -0800

    added move towards base script and test scene

--- Assets/Scripts/MoveTowardsBase.cs
+++ Assets/Scripts/MoveTowardsBase.cs
@@ -13,9 +13,36 @@ public class MoveTowardsBase : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 currentPos = transform.position;
-		Vector3 castlePos = castle.transform.position;
-		float step = speed * Time.deltaTime;
-		currentPos = Vector3.MoveTowards (currentPos, castlePos, step);
+		// if there's any targetable object in range, stop and call attack method
+		Collider target = GetClosestTargetable ();
+		if(target != null){
+			//call attack(target)		
+			Debug.Log("Attack");
+	}
+		// move towards enemy base
+	else {
+    Vector3 currentPos = transform.position;
+	Vector3 castlePos = castle.transform.position;
+	float step = speed * Time.deltaTime;
+	transform.position = Vector3.MoveTowards (currentPos, castlePos, step);
 	}
 }
+
+	
+	Collider GetClosestTargetable(){
+		float smallestDistance = float.MaxValue;
+		Collider closestTarget = null;
+			Collider[] cols = Physics.OverlapSphere(transform.position, .5f);
+			foreach (Collider col in cols){
+				if (col && col.tag == "Enemy"){ 
+				float distance = Vector3.Distance(transform.position, col.transform.position);
+				if(distance<smallestDistance){
+					smallestDistance = distance;
+					closestTarget = col;					
+				}			
+		}	
+	}
+			return closestTarget;
+}
+}
+
