5ab02b59aeab04f75232d3f1923668fb439686c4
Eric Kim <bosung90@gmail.com>
2015-02-13 02:24:11 -0800

    Adding game field on ID marker 12 (just flat plane for now) Will work on helath bar soon.

---
+++ Assets/Scripts/AlwaysFaceCamera.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlwaysFaceCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.LookAt (Camera.main.transform);
+		transform.RotateAround (transform.position, transform.up, 180f);
+	}
+}
---
+++ Assets/Scripts/HPBarDamageable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HPBarDamageable : MonoBehaviour {
+
+	Image healthBar;
+//	public Damageable damageable;
+	
+	void Start () {
+		healthBar = GetComponent<Image> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		healthBar.fillAmount = damageable.CurHealth / (float)damageable.MaxHealth;
+	}
+}
