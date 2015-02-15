cb0aaebcbb7658bc585722b46e3072a5e5e7f02a
Lisa Wong <miss.lisa19@hotmail.com>
2015-02-15 06:31:30 -0800

    Added script for Giant

---
+++ Assets/Scripts/GiantScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GiantScript : MonoBehaviour {
+	AudioSource giantSound;
+	// Use this for initialization
+	void Start () {
+		giantSound = this.GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!animation.isPlaying)
+			animation.Play("Idle_01",  PlayMode.StopAll);
+
+	}
+
+	void AttackAnimate(){
+		animation.Play ("Attack_01", PlayMode.StopAll);
+		giantSound.Play ();
+		}
+
+
+}
