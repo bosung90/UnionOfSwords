c8c1d3a86b14a9bc6bf5d26389e71ce0031f4fe1
Lisa Wong <miss.lisa19@hotmail.com>
2015-02-15 09:47:29 -0800

    Added ranger

---
+++ Assets/RangerScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RangerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(!animation.isPlaying)
+			animation.Play("001-01_idle01",  PlayMode.StopAll);
+	
+	}
+
+	void WalkAnimate(){
+		animation.Play("002_walk01",PlayMode.StopAll);
+		}
+}
